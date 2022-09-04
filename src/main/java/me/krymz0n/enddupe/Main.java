114c9a4c5b65dd88254c60cc47a240c92439e5ad
KRYMZ0N <jackgrimes05@yahoo.com>
2022-09-03 23:55:08 -0400

    still need to test!

--- src/main/java/me/krymz0n/enddupe/Main.java
+++ src/main/java/me/krymz0n/enddupe/Main.java
@@ -1,27 +1,39 @@
 package me.krymz0n.enddupe;
 
 import com.destroystokyo.paper.event.entity.EntityTeleportEndGatewayEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
 
     }
 
     @EventHandler
-    public void onTeleport(EntityTeleportEndGatewayEvent evt) { //TODO Need to look into abstract horse inv, and also how to actually do this lmfao.
+    public void onTeleport(EntityTeleportEndGatewayEvent evt) { //TODO Need to test
         if (isChestable(evt.getEntity())) {
             if (evt.getEntity() instanceof ChestedHorse) {
                 ChestedHorse ch = (ChestedHorse) evt.getEntity();
 
-                ch.getInventory();
+                Inventory inv = ch.getInventory();
+
+                if (ch.getHealth() < 1) {
+                   World w = Bukkit.getServer().getWorld(Objects.requireNonNull(evt.getTo()).getWorld().getUID());
+
+                    assert w != null;
+                    w.dropItemNaturally(evt.getTo(), new ItemStack((ItemStack) inv));
+                }
 
 
             }
